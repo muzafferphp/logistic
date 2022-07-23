572ee534267acbf0a5695e97f91a388727797d4b
muzaffer <mdmuzafferalam7860@gmail.com>
2022-07-23 16:34:22 +0530

    header chnage

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -20,8 +20,6 @@
   <link rel="stylesheet" href="{{asset('admin/css/vertical-layout-light/style.css')}}">
   <!-- endinject -->
   <link rel="shortcut icon" href="{{asset('admin/images/favicon.png')}}" />
-  
-  
   <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
